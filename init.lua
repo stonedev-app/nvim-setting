dfa8f07f6574f255f39cda0a7b75e38d61cd3719
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2024-07-02 22:49:07 +0900

    feat: Added line numbers, tab settings, lazy.nvim plugin manager, and nvim-tree (file explorer)

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- vim option
+local set = vim.opt
+-- line
+set.number = true
+-- tab
+set.tabstop = 2
+set.shiftwidth = 2
+set.expandtab = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+    {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      lazy = false,
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end,
+      keys = {
+          {mode = "n", "<C-n>", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree with Ctrl+n"},
+      },
+    },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
